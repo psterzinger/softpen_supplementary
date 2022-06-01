4447e8ab36c49ab867780310d47e2790469f5689
Philipp Sterzinger <philipp.sterzinger@warwick.ac.uk>
2022-06-01 19:31:00 +0100

    Auto commit: Mi  1 Jun 2022 19:31:00 BST

--- README.md
+++ README.md
@@ -2,11 +2,15 @@
 
 This repo contains all materials to the paper:  *Maximum softly-penalized likelihood for mixed effects logistic regression*.
 
-- **Data**: Data sets analysed in the Paper.
-- **Results**: Numerical results and figures from analyses and simulations in the paper and the supplementary material.
+- **Data**: Contains the Culcita data and the conditional inference data, that is analysed in Sections 3 and 8 in the paper. 
+- **Results**: Holds all numerical results and figures from analyses and simulations in the paper and the supplementary material.
 - **Scripts**: All scripts to recreate the numerical results in the paper. 
-  - **./Modelling** has all scripts that carry out the numerical analyses. 
-  - **./Software** contains all two scripts that carry out estimation, create figures and tables, and are called from the Scripts in Modelling. 
+	- **./Functions**: Contains the scripts `MSPAL.R` and `mv_MSPAL.R` which contain all functions used for the estimations and simulations as well as for the generation of figures and tables. 
+	- `culcita_sep.R`: Generates the estimates in Table 2 of the main text and Table S1 of the supplementary material. 
+	- `culcita_sim.R`: Conducts the simulation study in Section 3 of the main text and generates Figure 1.
+	- `cond_inf_example.R`: Conducts all estimates and the simulation of Section 8 in the main text. Generates Figure 2 of the paper and Figure S1 of the supplementary material, as well as Tables 3 and 4 of the main text and table S2 in the supplementary material. 
+	- `Sim_1.R`: Conducts the simulation study in Section S4.1 in the supplementary material and generates Figure S2 and Table S3 therein. 
+	- `Sim_2.R`: Conducts the simulation study in Section S4.2 in the supplementary material and generates Figure S3 and Table S4 therein. 
 - **softpen_supplementary.pdf**: Document with further material to the examples and simulations of the paper and additional simulation studies.
 
 Shield: [![CC BY 4.0][cc-by-shield]][cc-by]
---
+++ Scripts/culcita_sim.R
@@ -0,0 +1,88 @@
+## Distributed as part of the supplementary material for the manuscript
+## "Maximum softly-penalized likelihood for mixed effects logistic regression"
+##
+## Authors: Philipp Sterzinger, Ioannis Kosmidis
+## Date: 1 June 2022
+## Licence: GPL 2 or greater
+## NOT A POLISHED PIECE OF PUBLIC-USE SOFTWARE! Provided "as is".
+## NO WARRANTY OF FITNESS FOR ANY PURPOSE!
+library(numDeriv)
+library(lme4)
+library(blme)
+library(optimx)
+library(dplyr)
+library(parallel)
+library(doMC)
+library(ggplot2)
+library(patchwork)
+library(scales)
+
+functions_path <- "./Functions"
+data_path <- "../../Data"
+results_path <- "../../Results"
+figures_path <- results_path
+
+source(file.path(functions_path, "MSPAL.R"))
+load(file.path(data_path, "culcita.RData"))
+
+## Parameterization
+estimate_threshold <- 30
+se_threshold <- 30
+grad_threshold <- 0.001
+nAGQ <- 100
+
+## Prepare list with model matrix, response vector, weights, and grouping variable
+dat <- list(X = model.matrix(~ ttt, data = culcita_dat),
+            Y = culcita_dat$predation,
+            M = rep(1, nrow(culcita_dat)),
+            grouping = culcita_dat$block)
+
+## Get MAL
+fit <- glmm_fit(c(0, 0, 0, 0, 0), mult = c(0, 0), data = dat,
+                method = c("L-BFGS-B", "nlminb"), nAGQ = nAGQ)
+
+truth <- drop(coef(fit))
+
+out <- perform_experiment(truth = truth,
+                          data = dat,
+                          nsimu = 10000,
+                          seed = 123,
+                          alt_start = rep(0, length(truth)),
+                          nAGQ = nAGQ,
+                          optimization_methods = c("L-BFGS-B", "nlminb"),
+                          ncores = 10,
+                          mathpar = c("beta[0]",
+                          "beta[1]",
+                          "beta[2]",
+                          "beta[3]",
+                          "log(sigma)"))
+
+
+
+out_summary <- summarize_experiment(out,
+                                    estimate_threshold = estimate_threshold,
+                                    se_threshold = se_threshold,
+                                    grad_threshold = grad_threshold)
+
+#load(file.path(results_path, "simulation_study_example1.rda"))
+out$method <- ordered(
+  rep(c("ML", "bglmer[n]", "bglmer[t]", "MSPL"), each = 5),
+  levels = c("MSPL", "bglmer[t]", "bglmer[n]", "ML"))
+pdf("../../Documents/Figures/simulation_results.pdf", width = 8, height = 7)
+plot_experiment(out,
+                estimate_threshold = estimate_threshold,
+                se_threshold = se_threshold,
+                grad_threshold = grad_threshold,
+                xlims = c(-11, 11))
+dev.off()
+
+out %>%
+        group_by(method, sample) %>%
+        summarize(failed = any(is.na(estimate) |
+        is.na(se) | isTRUE(abs(estimate) > estimate_threshold) |
+        isTRUE(se > se_threshold) |
+        isTRUE(abs(grad) > grad_threshold))) %>%
+            summarize(mean(failed))
+
+
+save.image(file.path(results_path, "simulation_study_example1.rda"))
